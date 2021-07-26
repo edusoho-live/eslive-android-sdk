9784d5449f23fb65cff5ec6ad974b2e79e36f9e4
zhugexiaobo <zhugexiaobo@gmail.com>
2021-07-26 19:17:27 +0800

    feat: demo fix

--- app/src/main/java/com/example/livecloud/MainActivity.java
+++ app/src/main/java/com/example/livecloud/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.livecloud;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button enter = findViewById(R.id.button);
+        Switch type = findViewById(R.id.type);
         enter.setOnClickListener(view -> {
             String api = ((TextInputLayout)findViewById(R.id.apiText)).getEditText().getText().toString();
             String room = ((TextInputLayout)findViewById(R.id.roomText)).getEditText().getText().toString();
@@ -31,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
             if (api.endsWith("/")) {
                 api = api.substring(0, api.length() - 1);
             }
-            String url = api + "/h5/room/" + room + "/enter?inapp=1&token=" + token;
-            LiveCloudActivity.launch(MainActivity.this, url, true, null);
+            String url = api + (type.isChecked() ? "/h5/room/" : "/h5/replay/") + room + "/enter?inapp=1&token=" + token;
+            LiveCloudActivity.launch(MainActivity.this, url, type.isChecked(), null);
         });
     }
 
